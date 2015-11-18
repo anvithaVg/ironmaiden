1ec0b2726f4493b3ac8a24d192692a9f3fd96b18
Anvitha VG <aganesh@hyperglance.com>
2015-11-18 17:04:26 +0000

    Iron Maiden spotify

---
+++ karma.conf.js
@@ -0,0 +1,54 @@
+module.exports = function (config) {
+
+    config.set({
+
+        // list of files to load in the browser
+        files: [
+            'app/js/lib/angular.js',
+            'app/app.js',
+            'test/unit.js'
+        ],
+
+        // list of files to exclude
+        exclude: [],
+
+        proxies: {},
+
+        // web server port
+        port: 9876,
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+        autoWatch: true,
+        usePolling: true,
+
+        // frameworks to use
+        frameworks: ['jasmine'],
+        browsers : ['ChromeDesktop'],
+
+        customLaunchers: {
+            ChromeDesktop: {
+                base: 'Chrome',
+                flags: ['--window-size=1280,720']
+            }
+        },
+
+        plugins: [
+            'karma-jasmine',
+            'karma-chrome-launcher',
+            'karma-mocha-reporter'
+        ],
+
+        // Test results reporter to use
+        // possible values: 'dots', 'progress', 'mocha', 'junit', 'growl', 'coverage'
+        reporters: ['mocha'],
+
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true
+
+    });
+};
